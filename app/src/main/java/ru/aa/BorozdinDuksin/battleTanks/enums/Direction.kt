13a8ae7bcb2bee8d78d51232f492c8da785d89b4
Fubar03 <neupokoyev2004@mail.ru>
2025-05-30 00:57:18 +0300

    Save before reauthoring history

--- app/src/main/java/ru/aa/BorozdinDuksin/battleTanks/enums/Direction.kt
+++
@@ -1,8 +0,0 @@
-package ru.aa.BorozdinDuksin.battleTanks.enums
-
-enum class Direction(val rotation: Float) {
-    UP(0f),
-    DOWN(180f),
-    RIGHT(90f),
-    LEFT(270f),
-}
--- app/src/main/java/ru/aa/BorozdinDuksin/battleTanks/models/Coordinate.kt
+++
@@ -1,6 +0,0 @@
-package ru.aa.BorozdinDuksin.battleTanks.models
-
-data class Coordinate(
-    val top: Int,
-    val left: Int
-)
